8551205f20f3affadd8e60fbc41c0b4765e8d0d9
salute <2512183698@qq.com>
2021-03-26 11:59:32 +0800

    push code file

---
+++ code/save.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>提交到服务器的结果</title>
+  </head>
+
+  <body>
+    <h1> 提交到服务器的结果</h1>
+    <?php
+      foreach ($_POST as $key => $value) {
+        echo "<li>{$key} : {$value} </li>";
+      }
+    ?>
+
+  </body>
+  </html>
+
